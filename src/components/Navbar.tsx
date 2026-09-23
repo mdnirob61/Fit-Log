bf32b7f47e434a56e438499e0535c763debddb49
mdnirob61 <nirobstudy2004@gmail.com>
2026-09-23 23:37:02 +0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import logo from "@/assets/logo.png";
+
+const Navbar = () => {
+    const pathname = usePathname();
+
+    return (
+        <nav className="sticky top-0 z-50 w-full border-b border-[#202227] bg-black">
+            <div className="mx-auto flex h-20 max-w-[1280px] items-center justify-between px-6">
+
+                <Link href="/" className="flex items-center gap-2">
+                    <Image
+                        src={logo}
+                        alt="FitLog logo"
+                        width={30}
+                        height={30}
+                    ></Image>
+
+                    <div className="text-[1.3rem] font-bold tracking-wide text-white">
+                        FITLOG
+                    </div>
+                </Link>
+
+                <div className="absolute left-1/2 flex -translate-x-1/2 items-center gap-3">
+                    <Link
+                        href="/"
+                        className={`rounded-full px-4 py-1.5 text-[0.8rem] font-medium transition ${pathname === "/"
+                            ? "bg-[#ccff00] text-black"
+                            : "text-[#8b8d91] hover:text-white"
+                            }`}>
+                        Workouts
+                    </Link>
+
+                    <Link
+                        href="/my-plan"
+                        className={`rounded-full px-4 py-1.5 text-[0.8rem] font-medium transition ${pathname === "/my-plan"
+                            ? "bg-[#ccff00] text-black"
+                            : "text-[#8b8d91] hover:text-white"
+                            }`}>
+                        My Plan
+                    </Link>
+                </div>
+
+                <div className="flex items-center gap-4">
+                    <Link
+                        href="/my-plan"
+                        className="flex items-center gap-1 text-[#8b8d91] transition hover:text-white text-[0.8rem]">
+                        Plan
+
+                        <span className="flex h-4 min-w-4 items-center justify-center rounded-full bg-[#ccff00] px-1 text-[9px] font-semibold text-black">
+                            0
+                        </span>
+                    </Link>
+
+                    <Link
+                        href="/my-plan"
+                        className="flex items-center gap-1 text-[#8b8d91] transition hover:text-white text-[0.8rem]">
+                        Saved
+                        <span className="text-[#8b8d91]">0</span>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
